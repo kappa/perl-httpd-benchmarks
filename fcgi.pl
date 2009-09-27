b0531d6bd01e6460e7d1adc10d6cdbcccf542b1d
Sergey Homenkow <hsw@rambler.ru>
2009-09-27 19:16:41 +0400

    Clean FastCGI implementation.

---
+++ fcgi.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl -w
+
+# nginx.conf:
+# location /fcgisum {
+#     fastcgi_pass unix:/tmp/sum_fcgi.socket;
+# 
+#     fastcgi_param  PATH_INFO   $fastcgi_script_name;
+#     fastcgi_param  QUERY_STRING  $query_string;
+#     fastcgi_param  REQUEST_METHOD  $request_method;
+#     fastcgi_param  CONTENT_TYPE  $content_type;
+#     fastcgi_param  CONTENT_LENGTH  $content_length;
+# }
+
+use strict;
+use warnings;
+
+use FCGI;
+use CGI::Simple;
+
+my $sock_fn = '/tmp/sum_fcgi.socket';
+
+my $socket  = FCGI::OpenSocket($sock_fn, 100);
+eval { chmod 0777, $sock_fn };
+my $request = FCGI::Request(\*STDIN, \*STDOUT, \*STDERR, \%ENV, $socket, FCGI::FAIL_ACCEPT_ON_INTR());
+
+while ($request->Accept() >= 0) {
+    my $q = CGI::Simple->new;
+    print "Content-Type: text/plain\n\n".
+        ($q->param('a') + $q->param('b'));
+}
+
+FCGI::CloseSocket($socket);
