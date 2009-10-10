ae9e905f4173b00bce1d6a4c04428722349dccaa
Alex Kapranoff <kapranoff@gmail.com>
2009-10-11 00:20:23 +0400

    Inline handler, set content-type.

--- httpd-engine.pl
+++ httpd-engine.pl
@@ -11,16 +11,17 @@ my $engine = HTTP::Engine->new(
             host => 'localhost',
             port =>  8010,
         },
-        request_handler => \&handle_request,
+        request_handler => sub {
+            my $req = shift;
+            my $vars = $req->query_parameters;
+            my $res = HTTP::Engine::Response->new(
+                body => $vars->{a} + $vars->{b},
+                );
+            $res->headers->header('Content-Type' => 'text/plain');
+            return $res;
+        },
     },
-    );
+);
 
 $engine->run;
 
-sub handle_request {
-    my $req = shift;
-    my $vars = $req->query_parameters;
-    return HTTP::Engine::Response->new(
-        body => $vars->{a} + $vars->{b},
-        );
-}
